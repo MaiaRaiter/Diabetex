6f761f84f91d01e0fad9325af1fdb591f4f30325
SHIRLY JUSID <46875521@ort>
2023-05-10 16:52:49 -0300

    .

--- Diabetex.API/Models/Plan.cs
+++ Diabetex.API/Models/Plan.cs
@@ -1,7 +1,7 @@
 using System.Text.Json.Serialization; 
 namespace Diabetex.API.Models; 
 
-public class Id_Plan {
+public class Plan {
     public int Id_Plan { get; set; } 
     public int Id_Usuario { get; set; } 
     public bool Free { get; set; }
