fb44a38bca3169bdb607d6191bbae0b88ea9102d
MAIA RAITER <47129177@ort>
2023-06-02 11:47:11 -0300

    scanner

--- Frontend/diabetex/src/App.js
+++ Frontend/diabetex/src/App.js
@@ -2,11 +2,11 @@
 import './App.css';
 import React, { useState } from "react";
 import LoginPage from './Componentes/LoginPage';
- 
+ import Scanner from './Componentes/Scanner.js';
 const App = () => {
   return (
     <React.Fragment>
-        <LoginPage/>
+        <Scanner/>
     </React.Fragment>
   );
 }
---
+++ Frontend/diabetex/src/Componentes/Scanner.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import QrReader from 'react-qr-reader';
+
+const BarcodeScanner = () => {
+  const [barcode, setBarcode] = useState('');
+
+  const handleScan = (data) => {
+    if (data) {
+      setBarcode(data);
+    }
+  };
+
+  const handleError = (err) => {
+    console.error(err);
+  };
+
+  return (
+    <div>
+      <QrReader
+        delay={300}
+        onError={handleError}
+        onScan={handleScan}
+        style={{ width: '100%' }}
+      />
+      <p>{barcode && `Código de barras: ${barcode}`}</p>
+    </div>
+  );
+};
+
+export default BarcodeScanner;
+
