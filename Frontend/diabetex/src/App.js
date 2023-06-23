985c5e2e651958924cde14ef19961736ac6a19cb
MAIA RAITER <47129177@ort>
2023-06-23 10:42:24 -0300

    Producto

--- Frontend/diabetex/src/App.js
+++ Frontend/diabetex/src/App.js
@@ -1,17 +1,54 @@
 
 import './App.css';
 import React from "react";
+import axios from 'axios'
+import { useEffect } from 'react';
+import IniciarSesion from './Componentes/Screens/InciarSesion.js'
+import Producto from './Componentes/Screens/Producto';
 
-import Home from './Componentes/Screens/Home'
 
 
 const App = () => {
-  return (
-    <React.Fragment>
-    <Home/>
+  const CargarProductos = async() => {
+    const CodigoB =3175680011480;
+    axios
+      .get("http://a-phz2-cidi-021:3000/api/producto/" + CodigoB)
+      .then((result) => {
+        if (result.data.status === 1){
+          //setPost(result.data.product]);
+          console.log(result.data.brands);
+        }else{
+         // no se encontro
+         console.log('No se encontro');
+        }
+        
+      })
+      .catch((error) => {
+        //setPost(error);
+        console.log("Error");
+      });
+  }
+  
+  //const producto = result.data.results;
+  //var index= 0;
+
+  useEffect(() => {
+    CargarProductos()
+  }, []);
+
+ //<Producto onCargarProductos={CargarProductos} />
+
+    return (
+     
+     <React.Fragment>
+
+        <Producto/>
+      </React.Fragment>
+  
       
-    </React.Fragment>
-  );
+    );
+
+
+
 }
- 
 export default App;
--- Frontend/diabetex/src/Componentes/Screens/Producto.js
+++ Frontend/diabetex/src/Componentes/Screens/Producto.js
@@ -1,25 +1,127 @@
-import React from "react";
-import {axios} from axios
+import axios from 'axios'
 
-function getById(){
-    let id = document.getElementById('txtId').value;
-    //let url =(BASE_URL_PIZZAS + id);
-    console.log('getById: ', id);
+const Producto = () => {
+
+  const CargarProductoxCodigo = async() => {
+    const CodigoB =3175680011480;
     axios
-      .get("http://localhost:3000/api/pizzas/" + id)
+      .get("http://a-phz2-cidi-021:3000/api/producto/" + CodigoB)
       .then((result) => {
+        if (result.data.status === 1){
+          //setPost(result.data.product]);
+          console.log(result.data.brands);
+          MostrarProducto(result.data.brands,false)
+        }else{
+         // no se encontro
+         console.log('No se encontro');
+         MostrarProducto(result.data.brands,true)
+        }
         
-        //console.log(response.data);
-        //console.log(response.status);
-        //console.log(response.statusText);
-        //console.log(response.headers);
-        //console.log(response.config);
-        displayUnaPizza(result.data, false);
+      })
+      .catch((error) => {
+        //setPost(error);
+        console.log("Error");
+      });
+
+     
+const MostrarProducto = (producto, huboUnerror) =>
+{
+  const huboError=false;
+  if (huboError){
+  //Pantalla de que no se encontro el producto y que se agregue uno
+  }
+<p> ${producto.Nombre}</p>
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /*function getById(){
+   
+    //let url =(BASE_URL_PIZZAS + id);
+    console.log('getById: ');
+    axios
+      .get("http://a-phz2-cidi-021:3000/api/producto/3175680011480")
+      .then((result) => {
+        displayUnProducto(result.data, false);
       })
       .catch(error => {
-        displayUnaPizza({}, true);
+        displayUnProducto({}, true);
       });
+      function displayUnProducto(unProducto, huboError){
+        let estilo = 'table-dark';
+        if (huboError){
+          estilo = 'table-danger';
+        }
+        let table = '<table className="table table-striped table-hover">';
+        table += `<thead class="${estilo}"><tr><th class="col-1 text-center">Id</th><th class="col-3">Nombre</th><th class="col-5">Descripcion</th><th class="col-2 text-center">Importe</th><th class="col-1 text-center">Libre Gluten</th></tr></thead>`;
+          table += `<tr>`;
+          table += `<td scope="col" className="text-center">${unProducto.Id}</td>`;
+          table += `<td scope="col">${unProducto.Nombre}</td>`;
+          table += `<td scope="col">${unProducto.Ingredientes}</td>`;
+          table += `<td scope="col" className="text-center">${unProducto.Marca}</td>`;
+          table += `<td scope="col" className="text-center">${unProducto.LibreGluten}</td>`;
+          table += `</tr>`;
+          table += "</table>";
+        document.getElementById("listado").innerHTML = table;
+      }
   }
-       `
+*/
+  return (
 
-export default CargarProductos ;
+    <center><button type="button" className="btn btn-success boton" onClick={() => CargarProductoxCodigo()}> Buscar por Codigo de Barra</button> </center>
+
+  );
+  }
+  export default Producto;
