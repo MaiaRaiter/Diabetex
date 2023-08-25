04814d66e01137dac0fb065ae3f4adbbd16536e2
MAIA RAITER <47129177@ort>
2023-08-25 11:44:07 -0300

    Carrusel

--- Frontend/diabetex/src/Componentes/Carrusel.js
+++ Frontend/diabetex/src/Componentes/Carrusel.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Carousel } from 'react-responsive-carousel';
 
 
-export const Carrusel = () => {
+
+export const Carrusel = ({DatosCarrusel}) => {
   const IdUsuario = 5;
   const [Producto, setProducto] = useState(null);
   const [error, setError] = useState(false);
@@ -11,7 +11,7 @@ export const Carrusel = () => {
   useEffect(() => {
     const CargarProductosRecientes = async () => {
       try {
-        const response = await axios.get(`http://a-phz2-cidi-021:3000/api/accesoProducto/${IdUsuario}`);
+        const response = await axios.get(DatosCarrusel);
         if (response.data) {
           setProducto(response.data);
           setError(false);
@@ -24,6 +24,9 @@ export const Carrusel = () => {
       }
     };
 
+    
+    
+
     CargarProductosRecientes();
   }, []);
 
@@ -33,12 +36,14 @@ export const Carrusel = () => {
         <div>Error al cargar el producto</div>
       ) : Producto ? (
         <>
-          <div className="horizontal-products-container d-flex flex-row">
+          <div className="horizontal-products-container d-flex flex-row ">
+        
             {Producto.map((P) => (
-              <div key={P.id} className='ProductosRecientes d-flex flex-column mr-2 mx-auto'>
-                <img src={P.Foto} className="FotoProducto" alt=""></img>
-                <p className='mx-auto'>Nombre: {P.Nombre}</p>
+              <div key={P.id} className='ProductosRecientes d-flex flex-column mr-2 mx-auto mt-50'>
+                <img src={P.Foto} className="FotoCarrusel" alt=""></img>
+                <p className='mx-auto'>{P.Nombre}</p>
               </div>
+            
             ))}
           </div>
         </>
@@ -46,5 +51,9 @@ export const Carrusel = () => {
         <div>No hay productos escaneados recientes</div>
       )}
     </div>
+
+
+
+
   );
 };
--- Frontend/diabetex/src/Componentes/Screens/Home.js
+++ Frontend/diabetex/src/Componentes/Screens/Home.js
@@ -4,14 +4,14 @@ import '../../index.css'
 import { BiSearch } from "react-icons/bi";
 import { Link } from 'react-router-dom'
 import { useState } from 'react';
-import {DiGhostSmall} from "react-icons/di";
+import { DiGhostSmall } from "react-icons/di";
 import { Carrusel } from '../Carrusel.js';
 
-
-
 export const Home = () => {
-
+  const IdUsuario = 5;
   const [searchValue, setSearchValue] = useState('');
+  const MasLikeados = `http://a-phz2-cidi-021:3000/api/likesXProducto`;
+  const Recientes = `http://a-phz2-cidi-021:3000/api/accesoProducto/${IdUsuario}`;
 
   const handleSearchChange = (event) => {
     setSearchValue(event.target.value);
@@ -22,7 +22,6 @@ export const Home = () => {
     // Aquí puedes realizar acciones relacionadas con la búsqueda, como enviarla a una API o actualizar el estado de tu aplicación.
     console.log('Valor de búsqueda:', searchValue);
   };
-
 
   return (
     <>
@@ -39,19 +38,15 @@ export const Home = () => {
             value={searchValue}
             onChange={handleSearchChange}
           />
-        
-            <BiSearch type="submit" className='SearchButton' />
-            <DiGhostSmall  className='filters'/>
+          <BiSearch type="submit" className='SearchButton' />
+          <DiGhostSmall className='filters' />
         </form>
       </div>
-      <p>Recientes</p>
-<Carrusel />
-
-
+      <p className='TituloHome'>Recientes</p>
+      <Carrusel DatosCarrusel={MasLikeados} />
+      <Carrusel DatosCarrusel={Recientes} />
+     
       <Navbar />
-
     </>
-
-
-  )
+  );
 }
