3e2b7c1b77f6a309997fadde82b44b02b659b8e8
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-29 10:15:51 -0300

    .

--- Frontend/diabetex/src/Componentes/Scanner.js
+++ Frontend/diabetex/src/Componentes/Scanner.js
@@ -1,22 +1,84 @@
-import React from "react";
-import BarcodeScannerComponent from "react-qr-barcode-scanner";
+import React, { useState } from 'react';
+import Html5QrcodePlugin from './ScannerPlugin'
 
-function App() {
-  const [data, setData] = React.useState("Not Found");
+function filterResults (results) {
+    let filteredResults = [];
+    for (var i = 0; i < results.length; ++i) {
+        if (i === 0) {
+            filteredResults.push(results[i]);
+            continue;
+        }
+
+        if (results[i].decodedText !== results[i - 1].decodedText) {
+            filteredResults.push(results[i]);
+        }
+    }
+    return filteredResults;
+}
+
+const ResultContainerTable = ({ data }) => {
+    const results = filterResults(data);
+    return (
+        <table className={'Qrcode-result-table'}>
+            <thead>
+                <tr>
+                    <td>#</td>
+                    <td>Decoded Text</td>
+                    <td>Format</td>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    results.map((result, i) => {
+                        console.log(result);
+                        return (<tr key={i}>
+                            <td>{i}</td>
+                            <td>{result.decodedText}</td>
+                            <td>{result.result.format.formatName}</td>
+                        </tr>);
+                    })
+                }
+            </tbody>
+        </table>
+    );
+};
+
+const ResultContainerPlugin = (props) => {
+    const results = filterResults(props.results);
+    return (
+        <div className='Result-container'>
+            <div className='Result-header'>Scanned results ({results.length})</div>
+            <div className='Result-section'>
+                <ResultContainerTable data={results} />
+            </div>
+        </div>
+    );
+};
+
+const App = (props) => {
+  const [decodedResults, setDecodedResults] = useState([]);
+  const onNewScanResult = (decodedText, decodedResult) => {
+      console.log("App [result]", decodedResult);
+      setDecodedResults(prev => [...prev, decodedResult]);
+  };
 
   return (
-    <>
-      <BarcodeScannerComponent
-        width={500}
-        height={500}
-        onUpdate={(err, result) => {
-          if (result) setData(result.text);
-          else setData("Not Found");
-        }}
-      />
-      <p>{data}</p>
-    </>
+      <div className="App">
+          <section className="App-section">
+              <div className="App-section-title"> Html5-qrcode React demo</div>
+              <br />
+              <br />
+              <br />
+              <Html5QrcodePlugin
+                  fps={10}
+                  qrbox={250}
+                  disableFlip={false}
+                  qrCodeSuccessCallback={onNewScanResult}
+              />
+              <ResultContainerPlugin results={decodedResults} />
+          </section>
+      </div>
   );
-}
+};
 
 export default App;
