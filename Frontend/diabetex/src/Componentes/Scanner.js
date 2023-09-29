7a9e630626ebf2819333f15a6c535c00d3a6d6d5
MAIA RAITER <47129177@ort>
2023-09-29 09:35:53 -0300

    .

--- Frontend/diabetex/src/Componentes/Scanner.js
+++ Frontend/diabetex/src/Componentes/Scanner.js
@@ -1,70 +1,22 @@
-import React, { Component } from "react";
-import WebcamBarcodeScanner from "react-webcam-barcode-scanner";
-import axios from "axios";
+import React from "react";
+import BarcodeScannerComponent from "react-qr-barcode-scanner";
 
-export default class App extends Component {
-  state = {
-    showCam: false,
-    scannedValue: "",
-    name: "",
-    isValid: "",
-    validFrom: "",
-    validTo: "",
-    errors: [],
-  };
+function App() {
+  const [data, setData] = React.useState("Not Found");
 
-  handleScan = (scannedValue) => {
-    if (scannedValue) {
-      this.setState({ scannedValue });
-    }
-  };
-
-  handleError = (error) => {
-    console.error(error);
-  };
-
-  handleClick = () => {
-    this.setState({ showCam: !this.state.showCam, scannedValue: "" });
-  };
-
-  handleValidate = () => {
-    // Realiza la validación del valor escaneado (this.state.scannedValue) aquí.
-    // Actualiza los estados isValid, validFrom, validTo y errors según corresponda.
-  };
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}>
-          {this.state.showCam
-            ? "Cerrar cámara"
-            : "Abrir cámara"}
-        </button>
-        {this.state.showCam ? (
-          <div>
-            <WebcamBarcodeScanner
-              onUserMediaError={this.handleError}
-              onScan={this.handleScan}
-              scannerStyle={{ width: "100%" }}
-            />
-          </div>
-        ) : null}
-        <p>Valor escaneado:</p>
-        <textarea
-          rows="3"
-          cols="50"
-          value={this.state.scannedValue}
-          readOnly
-        />
-        <p>
-          <button
-            disabled={!this.state.scannedValue}
-            onClick={this.handleValidate}
-          >
-            Validar
-          </button>
-        </p>
-      </div>
-    );
-  }
+  return (
+    <>
+      <BarcodeScannerComponent
+        width={500}
+        height={500}
+        onUpdate={(err, result) => {
+          if (result) setData(result.text);
+          else setData("Not Found");
+        }}
+      />
+      <p>{data}</p>
+    </>
+  );
 }
+
+export default App;
