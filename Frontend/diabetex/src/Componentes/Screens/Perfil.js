efcfed8bc718b951b7519ad107b40ce9579c7cb0
SHIRLY JUSID <46875521@ort>
2023-11-01 17:24:19 -0300

    .

--- Frontend/diabetex/src/Componentes/Screens/Perfil.js
+++ Frontend/diabetex/src/Componentes/Screens/Perfil.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import {IoSettingsOutline} from "react-icons/io5"
-
+import {IoHeartOutline} from "react-icons/io5"
 
 export default function Perfil (usuario) {
         
   return (
     <>
-    < IoSettingsOutline className='setting-icon'/>
+    < IoSettingsOutline className='setting-icon icon'/>
     <img src="/img/logo.jpg" className="App-logo Posicion-logo" ></img>
-    <p className='Nombre-usuario'>{usuario.Nombre}</p>
+    <p className='Nombre-usuario'>Gonzalo Perez{usuario.Nombre}</p>
+    < IoHeartOutline className='heart-icon'/>
     </>   
   );
 }
