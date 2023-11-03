1bca64bc6703132ef9ee3d9d8f31ba76b7923187
SHIRLY JUSID <46875521@ort>
2023-11-03 09:44:24 -0300

    midelware

--- Frontend/diabetex/src/Componentes/Screens/Perfil.js
+++ Frontend/diabetex/src/Componentes/Screens/Perfil.js
@@ -3,7 +3,19 @@ import axios from 'axios';
 import {IoSettingsOutline} from "react-icons/io5"
 import {IoHeartOutline} from "react-icons/io5"
 
-export default function Perfil (usuario) {
+export default function Perfil () {
+
+  useEffect(() => {
+    axios
+      .get('http://a-phz2-cidi-020:3000/api/usuario/'+id)
+      .then((result) => {
+        setProduct(result.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+  }, [id]);
         
   return (
     <>
--- Frontend/diabetex/src/context/UsuarioContext.js
+++ Frontend/diabetex/src/context/UsuarioContext.js
@@ -1,40 +1,39 @@
 import React, { createContext, useState, useEffect } from "react";
+
 export const UsuarioContext = createContext();
 
 const UsuarioProvider = (props) => {
+
     const [usuario, setUsuario] = useState([]);
+    //const cantidadCarrito = carrito.length
 
     useEffect(() => {
-      /*if (localStorage.getItem("usuario") != null) {
-        let storage = localStorage.getItem("usuario")
-        //obtengo los datos JSON del localstorage y los guardo en un array
-        setUsuario(JSON.parse(storage))
-        }
-        */
-
-        const [products, setProducts] = useState([]);
- 
-
-  
-
-    axios
-      .get("http://a-phz2-cidi-020:3000/api/usuario/"+ "?idUsuario=1")
-      .then((result) => {
-        setProducts(result.data.products);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    },[usuarios])
-
+        
+      if (usuario != null) {
+        //voy a busacr mi usuario guardado
+        //let usuario = 
+        setUsuario(usuario)
+    }
+    },[])
+    
+    const EliminarSesion = (usuarioid) => {
+      setUsuario(
+        usuario.filter((usuario) => usuario.id !== usuarioid)
+      ); 
+    };
+    
     return (
         <UsuarioContext.Provider
           value={{
-            usuario
+            usuario,
+            EliminarSesion
           }}
         >
           {props.children}
         </UsuarioContext.Provider>
-      );    
+      );
+
+
+    
 }
 export default UsuarioProvider;
