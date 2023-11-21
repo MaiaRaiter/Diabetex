9fc0b74c2088a782e929795b0157187d044f06b5
MAIA RAITER <47129177@ort>
2023-11-21 10:42:09 -0300

    .

--- Frontend/diabetex/src/Componentes/CardLikeados.js
+++ Frontend/diabetex/src/Componentes/CardLikeados.js
@@ -3,50 +3,42 @@ import { BiHeart } from 'react-icons/bi'
 import { FavoritosContext } from '../Context/FavoritosContext';
 import { BiSolidHeart } from 'react-icons/bi'
 
+
+
 const CardLikeados = ({ producto }) => {
-
-    const { EliminarFavorito } = useContext(FavoritosContext);
-    const { AddFavorito } = useContext(FavoritosContext);
-    const {  isFavorite } = useContext(FavoritosContext);    
-    const {  favoritos } = useContext(FavoritosContext);  
-    console.log(favoritos)
-
-    const handlefav = (id) =>{
-
-
-    }
-
+    const { EliminarFavorito, AddFavorito, isFavorite, favoritos } = useContext(FavoritosContext);
+  
+    const handleFav = (id) => {
+      // Lógica adicional si es necesario
+    };
+  
     return (
-
-        <div className="CardFavs">
-            <img className="FotoCard" src={producto.Foto} />
-            
-            
-            {isFavorite(producto.Id) ? (
-                    <BiSolidHeart className='FavPerfil' onClick={() => EliminarFavorito(producto.Id)} />
-                  
-          ) : (
-           
-            <BiHeart className='FavPerfil' onClick={() => AddFavorito(producto)} />
-           
-          )
-          
-
-          /*
-          favoritos.map((f)=>{
-            <BiHeart className='icon-heart' onClick={() => AddFavorito(f.id)} />
-            
-            
-
-          })
-          */
-          }
-
-        </div>
-
-
-
+        <>
+        <div class="container">
+  <div class="row">
+    <div class="col-sm">
+    <div className="CardFavs">
+        <img className="FotoCard" src={producto.Foto} />
+  
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+  </div>
+</div>
+      
+        {isFavorite(producto.Id) ? (
+          <BiSolidHeart className='FavPerfil' onClick={() => EliminarFavorito(producto.Id)} />
+        ) : (
+          <BiHeart className='FavPerfil' onClick={() => AddFavorito(producto)} />
+        )}
+      </div>
+      </>
     );
-};
+  };
+
 
 export default CardLikeados;
