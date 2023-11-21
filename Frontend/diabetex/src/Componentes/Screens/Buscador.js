9dab0e9faf4881a88f0ce2bd08791cdd5cd4272d
SHIRLY JUSID <46875521@ort>
2023-11-21 09:37:23 -0300

    .

--- Frontend/diabetex/src/Componentes/Screens/Buscador.js
+++ Frontend/diabetex/src/Componentes/Screens/Buscador.js
@@ -1,60 +1,65 @@
 import axios from "axios"
 import React, { useState, useEffect } from 'react';
-import  {Navbar}  from "../Navbar";
+import { Navbar } from "../Navbar";
 
- const Buscador = () => {
-    const [productos, setProductos] = useState(null);
-    const [productosFiltrados, setProductosFiltrados] = useState([]);
+const Buscador = () => {
+  const [productos, setProductos] = useState(null);
+  const [buscando, setBuscando] = useState(true);
+  const [productosFiltrados, setProductosFiltrados] = useState([]);
 
-    useEffect(() => {
-        CargarProductos()
-      }, [])
-    
-      const CargarProductos = async () => {
-    
-        let url = "http://localhost:3000/api/producto";
-    
-        try {
-          const response = await axios.get(url);
-          if (response.data) {
-            setProductos(response.data);
-    
-          } else {
-    
-          }
-        } catch (error) {
-          console.log(error);
-    
-        }
-      };
-    
-      const searchByName = (e) => {
-        const searchText = e.target.value.toLowerCase();
-    
-        if (productos) {
-    
-          //filter
-          //productos.filter(funcion -> boolean)
-    
-          const productosFiltrados = productos.filter(product => {
-            const productName = product.Nombre ? product.Nombre.toLowerCase() : "Sin nombre";
-    
-            return productName.startsWith(searchText);
-          });
-          setProductosFiltrados(productosFiltrados);
-          //Tengo una lista de productos filtrados que se actualiza cuando busco uno nuevo
-        }
-      };
+  useEffect(() => {
+    CargarProductos()
+  }, [])
+
+  const CargarProductos = async () => {
+
+    let url = "http://localhost:3000/api/producto";
+
+    try {
+      const response = await axios.get(url);
+      if (response.data) {
+        setProductos(response.data);
+
+      } else {
+
+      }
+    } catch (error) {
+      console.log(error);
+
+    }
+  };
+
+  const searchByName = (e) => {
+    const searchText = e.target.value.toLowerCase();
+
+    if (productos) {
+
+      //filter
+      //productos.filter(funcion -> boolean)
+      if (searchText) {
+        const productosFiltrados = productos.filter(product => {
+          const productName = product.Nombre ? product.Nombre.toLowerCase() : "Sin nombre";
+
+          return productName.startsWith(searchText);
+        });
+        setProductosFiltrados(productosFiltrados);
+        //Tengo una lista de productos filtrados que se actualiza cuando busco uno nuevo
+      } else {
+        setBuscando(false);
+      }
+    }
+
+  };
   return (
     <>
-    <div>
-    <input onChange={(e) => searchByName(e)} id="inputFiltro" type='text' placeholder='search...' autoComplete='off' className="widget-search-bar"  />
-    {productosFiltrados.map(producto => <p>{producto.Nombre}</p>)}
-  </div>
-    
+      <div>
+        <input onChange={(e) => searchByName(e)} id="inputFiltro" type='text' placeholder='search...' autoComplete='off' className="widget-search-bar" />
+        {buscando && (
+          productosFiltrados.map(producto => <p className="listaBuscador" key={producto.id}>{producto.Nombre}</p>)
 
-
-    <Navbar />
+        )}
+      </div>
+      <Navbar />
     </>
   )
 }
@@ -69,4 +74,3 @@ export default Buscador;
 
 
 
-      
--- Frontend/diabetex/src/Componentes/Screens/Home.js
+++ Frontend/diabetex/src/Componentes/Screens/Home.js
@@ -10,48 +10,6 @@ export const Home = () => {
   const IdUsuario = 2;
   const MasLikeados = `http://a-phz2-cidi-023:3000/api/likesXProducto`;
   const Recientes = `http://a-phz2-cidi-023:3000/api/accesoProducto/${IdUsuario}`;
-  const [productos, setProductos] = useState(null);
-  const [productosFiltrados, setProductosFiltrados] = useState([]);
-
-  useEffect(() => {
-    CargarProductos()
-  }, [])
-
-  const CargarProductos = async () => {
-
-    let url = "http://localhost:3000/api/producto";
-
-    try {
-      const response = await axios.get(url);
-      if (response.data) {
-        setProductos(response.data);
-
-      } else {
-
-      }
-    } catch (error) {
-      console.log(error);
-
-    }
-  };
-
-  const searchByName = (e) => {
-    const searchText = e.target.value.toLowerCase();
-
-    if (productos) {
-
-      //filter
-      //productos.filter(funcion -> boolean)
-
-      const productosFiltrados = productos.filter(product => {
-        const productName = product.Nombre ? product.Nombre.toLowerCase() : "Sin nombre";
-
-        return productName.startsWith(searchText);
-      });
-      setProductosFiltrados(productosFiltrados);
-      //Tengo una lista de productos filtrados que se actualiza cuando busco uno nuevo
-    }
-  };
 
   return (
     <div className="App-header d-flex flex-column justify-content-between vh-100">
@@ -59,16 +17,12 @@ export const Home = () => {
            
         <center><img src="/img/logo.jpg" className="logoHome" alt=""></img></center>
         <br></br>
-
-        <div>
-          <input onChange={(e) => searchByName(e)} id="inputFiltro" type='text' placeholder='search...' autoComplete='off' className="widget-search-bar"  />
-          {productosFiltrados.map(producto => <p>{producto.Nombre}</p>)}
-        </div>
+        <Buscador />
       
         </center>
       <div className='Carruseles'>
         <p className='TituloHome'>Mas likeados</p>
-        <Buscador productos={productos} />
+       
         <Carrusel DatosCarrusel={MasLikeados} />
         <p className='TituloHome'>Recientes</p>
         <Carrusel DatosCarrusel={Recientes} />
